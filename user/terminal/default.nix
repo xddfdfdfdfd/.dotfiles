8f7d1ee9cd62e2b1b41f93231341a520782b51b3
paginated <paginated.me@protonmail.com>
2024-10-20 13:54:54 +0530

    dotfiles: user: foot: Set font family to SauceCodePro Nerd Font

--- user/terminal/default.nix
+++ user/terminal/default.nix
@@ -1,10 +1,24 @@
-{
+let
+  # Font Family
+  font = "SauceCodePro Nerd Font";
+
+  # Font Size
+  size = "10.25";
+in {
   # Configure Foot
   programs.foot = {
     enable = true;
     settings = {
       main = {
         term = "xterm-256color";
+
+        font = "${font}:size=${size}:style=SemiBold";
+        font-bold = "${font}:size=${size}:style=Bold";
+        font-italic = "${font}:size=${size}:style=Italic";
+        font-bold-italic = "${font}:size=${size}:style=Bold,Italic";
+        dpi-aware = "yes";
+        line-height = 19;
+        underline-offset = 19;
       };
     };
   };
