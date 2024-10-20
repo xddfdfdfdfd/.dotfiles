bff54e5ff4b9c5a5d12cc07c6588f94b82fa2634
paginated <paginated.me@protonmail.com>
2024-10-20 14:39:34 +0530

    dotfiles: user: starship: Set prompt character symbol on normal and vi mode

--- user/terminal/starship.nix
+++ user/terminal/starship.nix
@@ -3,5 +3,15 @@
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      character = {
+        error_symbol = "[](bold red)";
+        success_symbol = "[](bold green)";
+        vimcmd_symbol = "[](bold green)";
+        vimcmd_visual_symbol = "[](bold yellow)";
+        vimcmd_replace_symbol = "[](bold red)";
+        vimcmd_replace_one_symbol = "[](bold red)";
+      };
+    };
   };
 }
