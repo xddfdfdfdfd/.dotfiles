3658a85cdaee66747704bb68d621868530ecb9f5
paginated <paginated.me@protonmail.com>
2024-10-20 21:28:47 +0530

    dotfiles: user: firefox: Install EliverLara/firefox-nordic-theme

--- user/firefox/default.nix
+++ user/firefox/default.nix
@@ -1,6 +1,24 @@
 {
+  config,
+  pkgs,
+  ...
+}: let
+  # Firefox Nordic Theme
+  nordic = pkgs.fetchFromGitHub {
+    owner = "EliverLara";
+    repo = "firefox-nordic-theme";
+    rev = "287c6b4a94361a038d63f2fa7b62f9adfc08fe02";
+    hash = "sha256-6ccxZKVsbR2VQr5M3GpbZt3Nfe5RaroWQdYx/aiWB64=";
+  };
+in {
+  home.file.".mozilla/firefox/${config.programs.firefox.profiles.default.path}/chrome".source = nordic;
+
   # Configure Firefox
   programs.firefox = {
     enable = true;
+    profiles.default = {
+      isDefault = true;
+      extraConfig = builtins.readFile "${nordic}/configuration/user.js";
+    };
   };
 }
