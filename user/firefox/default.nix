190277bb5b18077f134e76f7e85b762f04a39781
paginated <paginated.me@protonmail.com>
2024-10-20 22:00:49 +0530

    dotfiles: user: firefox: Disable irritating first run stuff

--- user/firefox/default.nix
+++ user/firefox/default.nix
@@ -19,6 +19,23 @@ in {
     profiles.default = {
       isDefault = true;
       settings = {
+        # Disable irritating first run stuff
+        # Source: https://github.com/Misterio77/nix-config/blob/main/home/gabriel/features/desktop/common/firefox.nix#L32-L45
+        "browser.disableResetPrompt" = true;
+        "browser.download.panel.shown" = true;
+        "browser.feeds.showFirstRunUI" = false;
+        "browser.messaging-system.whatsNewPanel.enabled" = false;
+        "browser.rights.3.shown" = true;
+        "browser.shell.checkDefaultBrowser" = false;
+        "browser.shell.defaultBrowserCheckCount" = 1;
+        "browser.startup.homepage_override.mstone" = "ignore";
+        "browser.uitour.enabled" = false;
+        "startup.homepage_override_url" = "";
+        "trailhead.firstrun.didSeeAboutWelcome" = true;
+        "browser.bookmarks.restore_default_bookmarks" = false;
+        "browser.bookmarks.addedImportButton" = true;
+        "datareporting.policy.firstRunURL" = "";
+
         # Auto install extensions
         "extensions.autoDisableScopes" = 0;
       };
